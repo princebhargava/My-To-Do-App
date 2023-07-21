405ce317f74d56683352b1985a1ae2b771cd3da9
Prince Bhargava <princebhargavapr@gmail.com>
2023-07-21 23:26:40 +0530

    Implement the add button

--- gui.py
+++ gui.py
@@ -4,9 +4,11 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a To-Do:")
 input_box = sg.InputText(tooltip="Enter todo",key="Todo")
 add_button = sg.Button("ADD")
-
+listbox = sg.Listbox(values=functions.get_todos(),key="todos",
+                     enable_events=True,size=[45,10])
+edit_button = sg.Button("Edit")
 window = sg.Window('My To-Do App',
-                   [[label],[input_box,add_button]],
+                   [[label],[input_box,add_button],[listbox,edit_button]],
                    font=("Helvetica",25))
 while True:
     event,values = window.read()
@@ -18,6 +20,18 @@ while True:
             new_todo = values["Todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values["Todo"]
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['Todo'].update(value=values['todos'][0])
         case sg.WINDOW_CLOSED:
             break
 window.close()
