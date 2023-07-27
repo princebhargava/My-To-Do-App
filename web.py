fe0f941541e2eaeedf18d43f71a210b16268fabb
Prince Bhargava <princebhargavapr@gmail.com>
2023-07-27 23:10:27 +0530

    Implement "Compelete To-do items" Feature.

--- web.py
+++ web.py
@@ -15,9 +15,13 @@ st.subheader("This my To-do app.")
 
 st.write("This app is used to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label=" ", placeholder="Enter a new to-do.......",
               on_change=add_todo,key="new_todo")
-st.session_state
